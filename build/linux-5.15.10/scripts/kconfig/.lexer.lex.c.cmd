f319be32bbc96287c5f8b014b08fb7350bb8c961
Alen <luojiaming299@163.com>
2024-04-04 21:59:12 +0800

    Delete build folder and conig linux kernel.
    Signed-off-by: Alen <luojiaming299@163.com>

--- build/linux-5.15.10/scripts/kconfig/.lexer.lex.c.cmd
+++
@@ -1,1 +0,0 @@
-cmd_scripts/kconfig/lexer.lex.c := flex -oscripts/kconfig/lexer.lex.c -L /home/alen/Code/Firmware_QEMU_GDB/linux-5.15.10/scripts/kconfig/lexer.l
--- build/linux-5.15.10/scripts/kconfig/.parser.tab.h.cmd
+++
@@ -1,1 +0,0 @@
-cmd_scripts/kconfig/parser.tab.h := bison -o scripts/kconfig/parser.tab.c --defines=scripts/kconfig/parser.tab.h -t -l /home/alen/Code/Firmware_QEMU_GDB/linux-5.15.10/scripts/kconfig/parser.y
