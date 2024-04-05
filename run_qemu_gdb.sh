8ef832651be8c4b29506ad55cb6797c85953d556
Alen <luojiaming299@163.com>
2024-04-06 00:20:16 +0800

    Fixed the bug "The specified task cannot be tracked" when start GDB debug.
    Signed-off-by: Alen <luojiaming299@163.com>

--- run_qemu_gdb.sh
+++ run_qemu_gdb.sh
@@ -5,7 +5,9 @@
 # For a copy, see <https://opensource.org/licenses/MIT>.
 
 # Check necessary files
+echo "Checking necessary files..."
 make all
 
 # qemu-system-aarch64 -M virt -cpu cortex-a53 -nographic -smp 1 -m 256 -kernel build/linux-5.15.25/arch/arm64/boot/Image -append "console=ttyAMA0" -initrd build/initramfs.cpio.gz -monitor /dev/null -serial /dev/null -serial stdio -S -s
+echo "Running QEMU with GDB..."
 qemu-system-aarch64 -machine virt -cpu cortex-a53 -smp 1 -m 2G -kernel ./build/linux-5.15.10/arch/arm64/boot/Image -initrd ./build/busybox-1.36.1/_install/rootfs.img -append "nokaslr console=ttyAMA0" -nographic -S -s
