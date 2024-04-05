8a0f31c5274fb0bca5ec0d1633fc618f532e181e
Alen <luojiaming299@163.com>
2024-04-05 16:30:26 +0800

    Change the compile file of linux kernel.
    Signed-off-by: Alen <luojiaming299@163.com>

--- build_linux_5_15_10_kernel.sh
+++ build_linux_5_15_10_kernel.sh
@@ -4,12 +4,18 @@
 # This script is licensed under the terms of the MIT license.
 # For a copy, see <https://opensource.org/licenses/MIT>.
 
+# Store the current directory
+current_dir=$(pwd)
+
+# Change to the "linux-5.15.10" directory
+cd linux-5.15.10
+
 # Check if ".config" file exists in the "linux-5.15.10" directory
-if [ -f "linux-5.15.10/.config" ]; then
+if [ -f ".config" ]; then
   # Check the value of CONFIG_DEBUG_INFO
-  if grep -q "^CONFIG_DEBUG_INFO=y$" linux-5.15.10/.config; then
+  if grep -q "^CONFIG_DEBUG_INFO=y$" .config; then
     # Compile make command based on the number of available processors
-    make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j$(nproc) -C linux-5.15.10
+    make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j$(nproc)
     # Check the make exit code
     if [ $? -eq 0 ]; then
       echo "Linux kernel compiled successfully."
@@ -23,6 +29,9 @@ if [ -f "linux-5.15.10/.config" ]; then
   fi
 else
   echo "Error: '.config' file not found in the 'linux-5.15.10' directory."
-  echo "Try to run 'make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- menuconfig -C linux-5.15.10' to generate the '.config' file."
+  echo "Try to run 'make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- menuconfig' to generate the '.config' file."
   exit 1
 fi
+
+# Change back to the original directory
+cd "$current_dir"
